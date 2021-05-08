5c381aef94827781803dd7685e7c6109d9818029
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2021-05-08 04:48:25 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const $html = document.querySelector('html')
+const $checkbox = document.querySelector('#switch')
+
+$checkbox.addEventListener('change', function() {
+  $html.classList.toggle('modo-escuro')
+})
